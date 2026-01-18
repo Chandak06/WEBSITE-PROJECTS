363a2653c42d66700a75ceceaaaf1e05e95ec8a9
Harshit Chandak <chandakharshit06@gmail.com>
2026-01-19 00:59:13 +0530

    .

--- BIG_PROJECTS/video-calling-interview-platform/backend/src/server.js
+++ BIG_PROJECTS/video-calling-interview-platform/backend/src/server.js
@@ -1,25 +1,37 @@
-import express from 'express';
-import {ENV} from "./lib/env.js";   
-import path from 'path';
+import express from "express";
+import { ENV } from "./lib/env.js";
+import path from "path";
 import { fileURLToPath } from "url";
+import { connectDB } from "./lib/db.js";
+import {serve} from "inngest/express"
+import {inngest,functions} from './lib/inngest.js'
+import cors from 'cors'
+
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename); 
 
-const app=express();
+const app = express();
 
+app.use(express.json());
+app.use(cors({origin : ENV.CLIENT_URL,credentials : true}))
+app.use("/api/inngest",serve({client :inngest,functions}))
 
-app.get("/api",(req,res)=>{
-    res.status(200).json({message:"Server is running"});
-})
-
-if(ENV.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,'../../frontend/dist')));
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,'../../frontend/dist/index.html'));
-    })
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
 }
-app.listen(ENV.PORT,()=>{
-    console.log(`Server is running on port ${ENV.PORT}`);
-});
 
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log(`Server is running on port ${ENV.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error starting the server", error);
+  }
+};
+startServer();
