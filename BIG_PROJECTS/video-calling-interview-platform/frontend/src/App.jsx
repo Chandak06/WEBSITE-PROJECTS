a57d4682d619cb59bcc7b7fc9fecd02342d89319
Harshit Chandak <chandakharshit06@gmail.com>
2026-03-30 09:24:09 +0530

    .

--- BIG_PROJECTS/video-calling-interview-platform/frontend/src/App.jsx
+++ BIG_PROJECTS/video-calling-interview-platform/frontend/src/App.jsx
@@ -1,18 +1,33 @@
-import { Route, Routes } from "react-router";
-import HomePage from "./Pages/HomePage.jsx";
-import AboutPage from "./Pages/AboutPage.jsx";
-import { Navigate } from "react-router";
 import { useUser } from "@clerk/clerk-react";
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
 
-const App = () => {
-  const {isSignedIn}= useUser();
+import { Toaster } from "react-hot-toast";
+import DashboardPage from "./pages/DashboardPage";
+import ProblemPage from "./pages/ProblemPage";
+import ProblemsPage from "./pages/ProblemsPage";
+import SessionPage from "./pages/SessionPage";
+
+function App() {
+  const { isSignedIn, isLoaded } = useUser();
+
+  // this will get rid of the flickering effect
+  if (!isLoaded) return null;
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>     
-      <Route path="/about" element={<AboutPage/>}/>
-      <Route path="/problems" element={isSignedIn ? <ProblemsPage/> : <Navigate to="/"/>}/>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
+        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
+      </Routes>
+
+      <Toaster toastOptions={{ duration: 3000 }} />
+    </>
   );
-};
+}
 
 export default App;
--- BIG_PROJECTS/video-calling-interview-platform/frontend/src/main.jsx
+++ BIG_PROJECTS/video-calling-interview-platform/frontend/src/main.jsx
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
-import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key");
+  throw new Error("Missing Clerk Publishable Key");
 }
 
 const queryClient = new QueryClient();
@@ -21,9 +21,8 @@ createRoot(document.getElementById("root")).render(
       <QueryClientProvider client={queryClient}>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
-          <Toaster position="top-right" />
         </ClerkProvider>
       </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
